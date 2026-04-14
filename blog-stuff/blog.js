0f9fc5148ab9764a6ca9d76857f3ef93e5974b9a
andrew <az42hu@gmail.com>
2026-04-14 12:11:27 -0400

    webp to save space

--- blog-stuff/blog.js
+++ blog-stuff/blog.js
@@ -11,7 +11,7 @@ const posts = [
     title: "junior week 3/23 - 3/27",
     date: "mar. 26th, 2026",
     preview: "getting closer and closer",
-    img: "../../static/img/junior/26/current.jpeg",
+    img: "../../static/img/junior/26/current.webp",
     href: "blog-posts/junior/week_26.html",
     tags: ["keyboard, shop tasks"]
   },
@@ -19,7 +19,7 @@ const posts = [
     title: "junior week 3/16 - 3/19",
     date: "mar. 18th, 2026",
     preview: "waterjetting is fun. someone should buy us a new computer becuase it's fucking windows 7",
-    img: "../../static/img/junior/25/current.jpeg",
+    img: "../../static/img/junior/25/current.webp",
     href: "blog-posts/junior/week_25.html",
     tags: ["keyboard, shop tasks"]
   },
@@ -27,7 +27,7 @@ const posts = [
     title: "junior week 3/9 - 3/13",
     date: "mar. 12th, 2026",
     preview: "back to keycaps",
-    img: "../../static/img/junior/24/keys.jpeg",
+    img: "../../static/img/junior/24/keys.webp",
     href: "blog-posts/junior/week_24.html",
     tags: ["keyboard"]
   },
@@ -35,7 +35,7 @@ const posts = [
     title: "junior week 3/2 - 3/6",
     date: "mar. 5th, 2026",
     preview: "redesigning lol",
-    img: "../../static/img/junior/23/current.png",
+    img: "../../static/img/junior/23/current.webp",
     href: "blog-posts/junior/week_23.html",
     tags: ["keyboard"]
   },
@@ -43,7 +43,7 @@ const posts = [
     title: "junior week 2/25 - 2/27",
     date: "feb. 26th, 2026",
     preview: "locked in on the keyboard. gonna make out of aluminum soon omg!!!1",
-    img: "../../static/img/junior/22/full.png",
+    img: "../../static/img/junior/22/full.webp",
     href: "blog-posts/junior/week_22.html",
     tags: ["keyboard"]
   },
@@ -51,7 +51,7 @@ const posts = [
     title: "junior week 2/9 - 2/13",
     date: "feb. 12th, 2026",
     preview: "making steno keyboard bc i'm bored and the resin printer sucks",
-    img: "../../static/img/junior/21/pcb_fixed.png",
+    img: "../../static/img/junior/21/pcb_fixed.webp",
     href: "blog-posts/junior/week_21.html",
     tags: ["keyboard, sten,"]
   },
@@ -59,7 +59,7 @@ const posts = [
     title: "junior week 2/2 - 2/6",
     date: "feb. 5th, 2026",
     preview: "waiting on the printer so did some random stuff",
-    img: "../../static/img/junior/20/error2.png",
+    img: "../../static/img/junior/20/error2.webp",
     href: "blog-posts/junior/week_20.html",
     tags: ["keyboard, misc, reflow oven"]
   },
@@ -67,7 +67,7 @@ const posts = [
     title: "junior week 1/28 - 1/30",
     date: "jan. 29th, 2026",
     preview: "keycap problems",
-    img: "../../static/img/junior/19/progress.jpeg",
+    img: "../../static/img/junior/19/progress.webp",
     href: "blog-posts/junior/week_19.html",
     tags: ["keyboard"]
   },
@@ -75,7 +75,7 @@ const posts = [
     title: "junior week 1/21 - 1/23",
     date: "jan. 22nd, 2026",
     preview: "rant abt how much i hate sanding",
-    img: "../../static/img/junior/18/batch3.jpeg",
+    img: "../../static/img/junior/18/batch3.webp",
     href: "blog-posts/junior/week_18.html",
     tags: ["keyboard"]
   },
@@ -83,7 +83,7 @@ const posts = [
     title: "junior week 1/12 - 1/16",
     date: "jan. 15th, 2026",
     preview: "rant abt form 3 resin printer incoming",
-    img: "../../static/img/junior/17/clusterfuck.jpeg",
+    img: "../../static/img/junior/17/clusterfuck.webp",
     href: "blog-posts/junior/week_17.html",
     tags: ["keyboard"]
   },
@@ -91,7 +91,7 @@ const posts = [
     title: "junior week 12/23 - 1/9",
     date: "jan. 8th, 2026",
     preview: "pcb's came pcb's came pcb's came pcb's came pcb's came pcb's came pcb's came pcb's came pcb's came pcb's came pcb's came pcb's came",
-    img: "../../static/img/junior/16/full.jpeg",
+    img: "../../static/img/junior/16/full.webp",
     href: "blog-posts/junior/week_16.html",
     tags: ["keyboard"]
   },
@@ -99,7 +99,7 @@ const posts = [
     title: "junior week 12/15 - 12/19",
     date: "dec. 18th, 2025",
     preview: "stabilizers came + working on the case",
-    img: "../../static/img/junior/15/case.png",
+    img: "../../static/img/junior/15/case.webp",
     href: "blog-posts/junior/week_15.html",
     tags: ["keyboard"]
   },
@@ -107,7 +107,7 @@ const posts = [
     title: "junior week 12/8 - 12/12",
     date: "dec. 11th, 2025",
     preview: "low motivation",
-    img: "../../static/img/junior/14/wired.png",
+    img: "../../static/img/junior/14/wired.webp",
     href: "blog-posts/junior/week_14.html",
     tags: ["keyboard"]
   },
@@ -115,7 +115,7 @@ const posts = [
     title: "junior week 11/24 - 12/5",
     date: "dec. 4th, 2025",
     preview: "two weeks work bc thanksgiving week was legit 2.5 days",
-    img: "../../static/img/junior/13/pcb_finished.png",
+    img: "../../static/img/junior/13/pcb_finished.webp",
     href: "blog-posts/junior/week_13.html",
     tags: ["keyboard"]
   },
@@ -123,7 +123,7 @@ const posts = [
     title: "junior week 11/17 - 11/21",
     date: "nov. 20th, 2025",
     preview: "downfall",
-    img: "../../static/img/junior/12/full-front.jpeg",
+    img: "../../static/img/junior/12/full-front.webp",
     href: "blog-posts/junior/week_12.html",
     tags: ["macropad"]
   },
@@ -131,7 +131,7 @@ const posts = [
     title: "junior week 11/10 - 11/14",
     date: "nov. 10th, 2025",
     preview: "major updates. finally started pcb and almost done",
-    img: "../../static/img/junior/11/fixed.png",
+    img: "../../static/img/junior/11/fixed.webp",
     href: "blog-posts/junior/week_11.html",
     tags: ["macropad"]
   },
@@ -139,7 +139,7 @@ const posts = [
     title: "junior week 11/3 - 11/7",
     date: "nov. 6th, 2025",
     preview: "learning the kb2040 by using circuitpython",
-    img: "../../static/img/junior/10/wp.png",
+    img: "../../static/img/junior/10/wp.webp",
     href: "blog-posts/junior/week_10.html",
     tags: ["macropad"]
   },
@@ -147,7 +147,7 @@ const posts = [
     title: "junior week 10/27 - 10/30",
     date: "oct. 30th, 2025",
     preview: "creating a bomb and claiming that it's for a school project!",
-    img: "../../static/img/junior/9/final.png",
+    img: "../../static/img/junior/9/final.webp",
     href: "blog-posts/junior/week_9.html",
     tags: ["ap lang proj"]
   },
@@ -155,7 +155,7 @@ const posts = [
     title: "junior week 10/20 - 10/24",
     date: "oct. 23rd, 2025",
     preview: "printer stuff + macropad stuff bc it's been 3 weeks",
-    img: "../../static/img/junior/8/full.png",
+    img: "../../static/img/junior/8/full.webp",
     href: "blog-posts/junior/week_8.html",
     tags: ["printer", "macropad"]
   },
@@ -163,7 +163,7 @@ const posts = [
     title: "junior week 10/14 - 10/17",
     date: "oct. 16th, 2025",
     preview: "fixing a printer because nobody wants to spend $2k to buy a replacement motherboard",
-    img: "../../static/img/junior/7/problem.jpeg",
+    img: "../../static/img/junior/7/problem.webp",
     href: "blog-posts/junior/week_7.html",
     tags: ["printer"]
   },
@@ -171,7 +171,7 @@ const posts = [
     title: "junior week 10/6 - 10/10",
     date: "oct. 9th, 2025",
     preview: "macropad work. new project",
-    img: "../../static/img/junior/6/pcb.png",
+    img: "../../static/img/junior/6/pcb.webp",
     href: "blog-posts/junior/week_6.html",
     tags: ["macropad"]
   },
@@ -179,7 +179,7 @@ const posts = [
     title: "junior week 9/29 - 10/2",
     date: "oct. 1st, 2025",
     preview: "almost done yippie",
-    img: "../../static/img/junior/5/finished_seven_s.jpeg",
+    img: "../../static/img/junior/5/finished_seven_s.webp",
     href: "blog-posts/junior/week_5.html",
     tags: ["7-seg"]
   },
@@ -187,7 +187,7 @@ const posts = [
     title: "junior week 9/22 - 9/26",
     date: "sep 25th, 2025",
     preview: "i'm just a slave to doing tasks that the shop gives us",
-    img: "../../static/img/junior/4/home.png",
+    img: "../../static/img/junior/4/home.webp",
     href: "blog-posts/junior/week_4.html",
     tags: ["shop tasks"]
   },
@@ -195,7 +195,7 @@ const posts = [
     title: "junior week 9/15 - 9/19",
     date: "sep 18th, 2025",
     preview: "even more work on my seven segment for this stupid sign lol",
-    img: "../../static/img/junior/3/schem.png",
+    img: "../../static/img/junior/3/schem.webp",
     href: "blog-posts/junior/week_3.html",
     tags: ["7-seg"]
   },
@@ -203,7 +203,7 @@ const posts = [
     title: "junior week 9/8 - 9/12",
     date: "sep 11th, 2025",
     preview: "more work on my seven segment for this stupid sign lol",
-    img: "../../static/img/junior/2/full.jpeg",
+    img: "../../static/img/junior/2/full.webp",
     href: "blog-posts/junior/week_2.html",
     tags: ["7-seg"]
   },
@@ -211,7 +211,7 @@ const posts = [
     title: "junior week 9/2 - 9/5",
     date: "sep 4th, 2025",
     preview: "helped around the shop, and reimaged a few computers",
-    img: "../../static/img/junior/1/broken.jpeg",
+    img: "../../static/img/junior/1/broken.webp",
     href: "blog-posts/junior/week_1.html",
     tags: ["shop tasks", "7-seg"]
   },
@@ -219,7 +219,7 @@ const posts = [
     title: "soph week 6/9 - 6/13",
     date: "jun 12th, 2025",
     preview: "helped around the shop, broke a few things on my computer somehow",
-    img: "../../static/img/soph/19/bsod.jpeg",
+    img: "../../static/img/soph/19/bsod.webp",
     href: "blog-posts/soph/week_19.html",
     tags: ["shop tasks"]
   },
@@ -227,7 +227,7 @@ const posts = [
     title: "soph week 6/2 - 6/6",
     date: "jun 5th, 2025",
     preview: "my final zerømouse post. i decided to scrap the project after this",
-    img: "../../static/img/soph/18/right_problems.PNG",
+    img: "../../static/img/soph/18/right_problems.webp",
     href: "blog-posts/soph/week_18.html",
     tags: ["zerømouse"]
   },
@@ -235,7 +235,7 @@ const posts = [
     title: "soph week 5/26 - 5/30",
     date: "may 30th, 2025",
     preview: "zerømouse updates! yaayayya i definitely don't hate my life!",
-    img: "../../static/img/soph/17/problem.png",
+    img: "../../static/img/soph/17/problem.webp",
     href: "blog-posts/soph/week_17.html",
     tags: ["zerømouse"]
   },
@@ -243,7 +243,7 @@ const posts = [
     title: "soph week 5/12 - 5/16",
     date: "may 17th, 2025",
     preview: "recap of the final moments before sumobot competition. end of sumobot.",
-    img: "../../static/img/soph/16/broken.jpeg",
+    img: "../../static/img/soph/16/broken.webp",
     href: "blog-posts/soph/week_16.html",
     tags: ["sumobot"]
   },
@@ -251,7 +251,7 @@ const posts = [
     title: "soph week 5/5 - 5/9",
     date: "may 8th, 2025",
     preview: "restarting because i'm stupid. zerømouse updates yet again",
-    img: "../../static/img/soph/15/error.jpeg",
+    img: "../../static/img/soph/15/error.webp",
     href: "blog-posts/soph/week_15.html",
     tags: ["zerømouse"]
   },
@@ -259,7 +259,7 @@ const posts = [
     title: "soph week 4/28 - 5/2",
     date: "may 1st, 2025",
     preview: "may have broke the pcb on the mouse i was using... zerømouse updates",
-    img: "../../static/img/soph/14/soulderfront.jpeg",
+    img: "../../static/img/soph/14/soulderfront.webp",
     href: "blog-posts/soph/week_14.html",
     tags: ["zerømouse"]
   },
@@ -267,7 +267,7 @@ const posts = [
     title: "soph week 4/14 - 4/18",
     date: "apr. 16th, 2025",
     preview: "made the grip for the zerømouse. won't be here tomorrow, so earlier blog post",
-    img: "../../static/img/soph/13/full_3d.png",
+    img: "../../static/img/soph/13/full_3d.webp",
     href: "blog-posts/soph/week_13.html",
     tags: ["zerømouse"]
   },
@@ -275,7 +275,7 @@ const posts = [
     title: "soph week 4/7 - 4/10",
     date: "apr. 10th, 2025",
     preview: "zerømouse and sumobot updates. primarily zerømouse.",
-    img: "../../static/img/soph/12/full.png",
+    img: "../../static/img/soph/12/full.webp",
     href: "blog-posts/soph/week_12.html",
     tags: ["sumobot", "mouse"]
   },
@@ -283,7 +283,7 @@ const posts = [
     title: "soph week 4/1 - 4/4",
     date: "apr. 3rd, 2025",
     preview: "made triggers for my zerømouse and test printed new bases.",
-    img: "../../static/img/soph/11/new.png",
+    img: "../../static/img/soph/11/new.webp",
     href: "blog-posts/soph/week_11.html",
     tags: ["zerømouse"]
   },
@@ -291,7 +291,7 @@ const posts = [
     title: "soph week 3/24 - 3/28",
     date: "mar. 27th, 2025",
     preview: "sumobot schematic and zerømouse updates.",
-    img: "../../static/img/soph/10/pcb_fixed.png",
+    img: "../../static/img/soph/10/pcb_fixed.webp",
     href: "blog-posts/soph/week_10.html",
     tags: ["sumobot", "mouse"]
   },
@@ -299,7 +299,7 @@ const posts = [
     title: "soph week 3/17 - 3/21",
     date: "mar. 20th, 2025",
     preview: "started creating the pcb for the sumobot. gonna get it milled soon",
-    img: "../../static/img/soph/9/schem.png",
+    img: "../../static/img/soph/9/schem.webp",
     href: "blog-posts/soph/week_9.html",
     tags: ["sumobot"]
   },
@@ -323,7 +323,7 @@ const posts = [
     title: "soph week 2/3 - 2/7",
     date: "feb. 7th, 2025",
     preview: "finishing touches of the acrylic piece of my lightbox, and now it's done.",
-    img: "../../static/img/soph/6/full.jpeg",
+    img: "../../static/img/soph/6/full.webp",
     href: "blog-posts/soph/week_6.html",
     tags: ["lightbox"]
   },
@@ -331,7 +331,7 @@ const posts = [
     title: "soph week 1/27 - 1/31",
     date: "jan. 30th, 2025",
     preview: "working on the pcb of my lightbox, and started the acrylic piece.",
-    img: "../../static/img/soph/5/error_1.png",
+    img: "../../static/img/soph/5/error_1.webp",
     href: "blog-posts/soph/week_5.html",
     tags: ["lightbox"]
   },
@@ -339,7 +339,7 @@ const posts = [
     title: "soph week 1/20 - 1/24",
     date: "jan. 23rd, 2025",
     preview: "finished the base of my lightbox. just gotta make the pcb and acrylic pieces now.",
-    img: "../../static/img/soph/4/full.png",
+    img: "../../static/img/soph/4/full.webp",
     href: "blog-posts/soph/week_4.html",
     tags: ["lightbox"]
   },
@@ -347,7 +347,7 @@ const posts = [
     title: "soph week 1/13 - 1/17",
     date: "jan. 16th, 2025",
     preview: "finished most of the base, fixing the code.",
-    img: "../../static/img/soph/3/3d.png",
+    img: "../../static/img/soph/3/3d.webp",
     href: "blog-posts/soph/week_3.html",
     tags: ["lightbox"]
   },
@@ -355,7 +355,7 @@ const posts = [
     title: "soph week 1/2 - 1/10",
     date: "jan. 9th, 2025",
     preview: "started a rough pcb and a sample box. bugs persist in my code.",
-    img: "../../static/img/soph/2/pcb.png",
+    img: "../../static/img/soph/2/pcb.webp",
     href: "blog-posts/soph/week_2.html",
     tags: ["lightbox"]
   },
@@ -363,7 +363,7 @@ const posts = [
     title: "soph week 12/16 - 12/20",
     date: "dec. 19th, 2024",
     preview: "figuring out the mechanism for my lightbox, and starting the code.",
-    img: "../../static/img/soph/1/sw1.jpeg",
+    img: "../../static/img/soph/1/sw1.webp",
     href: "blog-posts/soph/week_1.html",
     tags: ["lightbox"]
   }
@@ -374,8 +374,10 @@ function renderPost(post) {
   article.className = "blog-card";
   article.dataset.tags = post.tags.join(", ");
 
+  const imgPath = post.img.replace(/\.(jpeg|jpg|png|PNG)$/i, '.webp');
+
   article.innerHTML = `
-    <img src="${post.img}" alt="">
+    <img src="${imgPath}" alt="">
     <div class="blog-content">
         <h2>${post.title}</h2>
         <p class="date">${post.date}</p>
@@ -406,7 +408,7 @@ function initMeta() {
       }
     }
 
-    const today = new Date().toISOString().slice(0, 10); // "YYYY-MM-DD"
+    const today = new Date().toISOString().slice(0, 10);
 
     if (cached && cached.date === today) {
       updateMeta(post, cached.wordCount, cached.readingTime, tags);
@@ -449,13 +451,10 @@ function updateMeta(post, wordCount, readingTime, tags) {
   `;
 }
 
-// ----- 4. Bootstrapping: render cards, then init meta -----
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.getElementById("blog-grid");
 
-  // render all posts
   posts.forEach(post => grid.appendChild(renderPost(post)));
 
-  // now that .blog-card elements exist, run your meta logic
   initMeta();
 });
